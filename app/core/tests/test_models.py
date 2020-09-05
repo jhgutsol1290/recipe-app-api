1577918271b90ef18bac274253c371dd3c371862
jhgutsol1290 <jhgutsolpam@gmail.com>
2020-09-05 00:37:34 -0500

    fix: minor typo

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -65,7 +65,7 @@ class ModelTests(TestCase):
 
     def test_recipe_str(self):
         """Test the recipe string representation"""
-        recipe = model.Recipe.objects.create(
+        recipe = models.Recipe.objects.create(
             user=sample_user(),
             title='Steak and mushroom sauce',
             time_minutes=5,
---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,18 @@
+# from django.contrib.auth import get_user_model
+# from django.test import TestCase
+# from django.urls import reverse
+
+# from rest_framework import status
+# from rest_framework.test import APIClient
+
+# from core.models import Recipe
+
+# from recipe.serializers import RecipeSerializer
+
+
+# RECIPES_URL = reverse('recipe:recipe-list')
+
+
+# def sample_recipe(user, **params):
+#     """Create and return a sample recipe"""
+#     pass
